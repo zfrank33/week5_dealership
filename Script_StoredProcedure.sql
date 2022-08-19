584fb2665bab8945e06f5ad1eceb7909329bf0d4
Francisco Guadarrama <107962016+zfrank33@users.noreply.github.com>
2022-08-19 09:23:34 -0500

    Add files via upload

---
+++ Script_StoredProcedure.sql
@@ -0,0 +1,19 @@
+----------------------
+-- Stored Procedure: Report of total amount of money sold, grouped by brand name.
+----------------------
+
+USE Car_Dealership;
+
+DELIMITER //
+CREATE PROCEDURE Report_TotalSold_ByBrand ()
+BEGIN
+	SELECT C.Brand, SUM(I.Amount) AS TOTAL_AMOUNT_MONEY
+	FROM Invoice I
+	INNER JOIN Car_ForSelling C
+	ON I.Car_ForSellingID = C.ID
+	GROUP BY C.Brand;
+END //
+DELIMITER ;
+
+-- Example
+CALL Report_TotalSold_ByBrand;
